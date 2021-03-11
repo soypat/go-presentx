8b4c4eb81b357d5f627f124df270e23ca24eeb96
soypat <graded.sp@gmail.com>
2021-03-11 09:23:08 -0300

    finishing touches

---
+++ slides/_whygosrc/concurrent.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// THREAD_S OMIT
+	var ca,cb = make(chan bool),make(chan bool)
+	for {
+		go func() {
+			fmt.Println(<-ca)
+			cb<-true
+		}
+		go func() {
+			ca<-false
+			fmt.Println(<-cb)
+		}
+	}
+	// THREAD_E OMIT
+}
---
+++ slides/_whygosrc/helloworld.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message = "Hello World"
+	fmt.Println(message)
+}
